8fb1b10646825d61b3e938ca75507ad55a6198f4
Foster Houlston <fosterhoulston@Mac.attlocal.net>
2025-11-04 11:53:30 -0800

    local server connection created | endpoints created

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "<h1>This is the home page</h1>"
+
+@app.route('/login')
+def login():
+    return "<h1>This is the Spotify login page</h1>"
+
+@app.route('/uploadImage')
+def uploadImage():
+    return "<h1>This page is for upoading an image</h1>"
+
+@app.route('/takePhoto')
+def takePicture():
+    return "<h1>This page is for using a camera to upload an image</h1>"
+
+@app.route('/playlists')
+def playlists():
+    return "<h1>This page displays all playlists created with the app for the current spotify user</h1>"
+
+@app.route('/playlists/<playlistName>')
+def newPlaylist():
+    return "<h1>This page displays a specific playlist</h1>"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5555, debug=True)
